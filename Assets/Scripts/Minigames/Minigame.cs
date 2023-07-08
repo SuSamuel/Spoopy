13c28b46e625b97baaad0e3e89889e4840e9b14a
stanley.chan <stanley.chan185@gmail.com>
2023-07-07 23:25:15 -0400

    Update Minigame.cs

--- Assets/Scripts/Minigames/Minigame.cs
+++ Assets/Scripts/Minigames/Minigame.cs
@@ -9,7 +9,7 @@ public class Minigame : MonoBehaviour
         - create MinigameCollider - attach this script to an empty game object under Map > Minigame Triggers with a BoxCollider2D and RigidBody2D and move the collider to wherever on the map the minigame can be triggered. 
                             set the minigameID to the index that this minigame will be on the MInigameManager
         - create Minigame prefab - this is the UI element to be instantiated under the Minigame UI object (see Keys Minigame prefab under Prefabs > Minigames for an example), attach this Minigame script to the parent
-        - add the Minigame prefab to the list of minigames under MinigameManager
+        - add the Minigame prefab to the list of minigames under MinigameManager (ensuring that the index matches the id of the minigame (maybe find a better way to do this?))
     **/
 
     private MinigameManager minigameManager;
